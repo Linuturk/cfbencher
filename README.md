6755f6e63c77c0417221331fb581b14eb9a8af84
Justin Phelps <linuturk@onitato.com>
2013-07-19 10:30:26 -0500

    Inital Commit

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ cfbencher
 =========
 
 Rackspace cloud files testing tool.
+
+Simple script to flood a cloud files container with requests.
+You can upload a random set of files, fetch random files from
+a container, or cleanup the container (this removes all objects
+and the container itself.)
+Argparse makes repeated tests easy.
---
+++ cfbench.py
@@ -0,0 +1,144 @@
+#/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pyrax
+import time
+import random
+import argparse
+
+
+def connection_test(cf, container):
+    '''
+    Test connection by getting metadata and creating a test
+    container.
+    '''
+    try:
+        cf.create_container(container)
+    except:
+        print("Container creation failed.")
+    try:
+        cf.delete_container(container)
+    except:
+        print("Container deletion failed.")
+
+
+def upload_random_obj(cf, container, length):
+    '''
+    Generate n random objects and upload to container.
+    '''
+    cf.create_container(container)
+    text = pyrax.utils.random_name(length=length)
+    name = pyrax.utils.random_name(ascii_only=True)
+    chksum = pyrax.utils.get_checksum(text)
+    print("Uploading: {0}".format(name))
+    obj = cf.store_object(container, name, text, etag=chksum)
+    if chksum != obj.etag:
+        print("Checksum Mismatch!")
+
+
+def fetch_random_obj(cf, container):
+    '''
+    Fetch a random object.
+    '''
+    cont = cf.get_container(container)
+    objects = cont.get_objects()
+    num = random.randrange(0, len(objects))
+    obj = cont.get_object(objects[num].name)
+    return obj
+
+
+def upload_benchmark(cf, container, lengths, n):
+    '''
+    Upload n number of objects to a container.
+    '''
+    start = time.time()
+    for length in lengths:
+        for i in range(0, n):
+            upload_random_obj(cf, container, length)
+    end = time.time()
+    total_obj = n * len(lengths)
+    seconds = end - start
+    print("Uploaded {0} objects in {1} seconds.".format(total_obj, seconds))
+    print("{0} objects per second.".format(total_obj / seconds))
+
+
+def fetch_benchmark(cf, container, n, chunk_size=8192):
+    '''
+    Fetch n number of objects to a temp directory.
+    '''
+    start = time.time()
+    count = 0
+    mismatch = 0
+    while count < n:
+        obj = fetch_random_obj(cf, container)
+        obj_gen = obj.fetch(chunk_size=chunk_size)
+        output = "".join(obj_gen)
+        chksum = pyrax.utils.get_checksum(output)
+        if chksum == obj.etag:
+            match = "Matched"
+        else:
+            match = "Mismatch!"
+            mismatch += 1
+        print("Fetched: {0} Chksum: {1}".format(obj.name, match))
+        count += 1
+    end = time.time()
+    total_obj = n
+    seconds = end - start
+    print("Fetched {0} objects in {1} seconds.".format(total_obj, seconds))
+    print("{0} objects per second.".format(total_obj / seconds))
+    print("{0} mismatched checksums.".format(mismatch))
+
+
+def cleanup(cf, container):
+    '''
+    Cleanup all the testing data and containers.
+    '''
+    cont = cf.create_container(container)
+    total_obj = len(cont.get_objects())
+    start = time.time()
+    cont.delete_all_objects()
+    while cont.get_objects() != []:
+        print(cont.get_objects())
+    cont.delete()
+    end = time.time()
+    seconds = end - start
+    print("Deleted {0} objects in {1} seconds".format(total_obj, seconds))
+    print("{0} objects per second.".format(total_obj / seconds))
+
+if __name__ == "__main__":
+
+    # Setup pyrax connection handler
+    pyrax.set_setting("identity_type", "rackspace")
+    pyrax.set_setting("region", "ORD")
+    pyrax.set_credential_file(".pyrax_creds")
+    cf = pyrax.cloudfiles
+
+    # Command line arguments
+    description = "Benchmark Cloud Files."
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument('test', choices=['fetch', 'upload', 'clean'])
+    parser.add_argument('container', type=str,
+                        help='Container to use for tests.')
+    parser.add_argument('-n', '--count', type=int, default=10,
+                        help='Number of tests to perform.')
+    parser.add_argument('-c', '--chunk', type=int, default=8192,
+                        help='Chunk size for objects')
+    args = parser.parse_args()
+
+    # Testing variables
+    container = args.container
+    count = args.count
+    lengths = [args.chunk*10, args.chunk*100]
+
+    # Test connection
+    connection_test(cf, pyrax.utils.random_name(ascii_only=True))
+
+    if args.test == 'upload':
+        # Generate some random objects
+        upload_benchmark(cf, container, lengths, count)
+    elif args.test == 'fetch':
+        # Fetch random objects
+        fetch_benchmark(cf, container, count)
+    elif args.test == 'clean':
+        # Cleanup our mess
+        cleanup(cf, container)
